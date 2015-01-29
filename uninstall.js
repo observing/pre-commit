0b85343dd7d348ff7eb12326f1cd6b0a7b01ba53
Arnout Kazemier <info@3rd-Eden.com>
2015-01-30 00:12:15 +0100

    [minor] Clean-up the uninstall script

--- uninstall.js
+++ uninstall.js
@@ -1,28 +1,20 @@
 'use strict';
 
-var path = require('path')
-  , fs = require('fs');
-
-//
-// Compatibility with older node.js.
-//
-var existsSync = fs.existsSync || path.existsSync;
-
-//
-// The location of the pre-commit hook.
-//
-var precommit = path.resolve(__dirname, '../..', '.git', 'hooks', 'pre-commit');
+var fs = require('fs')
+  , path = require('path')
+  , exists = fs.existsSync || path.existsSync
+  , precommit = path.resolve(__dirname, '../..', '.git', 'hooks', 'pre-commit');
 
 //
 // Bail out if we don't have pre-commit file, it might be removed manually.
 //
-if (!existsSync(precommit)) return;
+if (!exists(precommit)) return;
 
 //
 // If we don't have an old file, we should just remove the pre-commit hook. But
 // if we do have an old precommit file we want to restore that.
 //
-if (!existsSync(precommit +'.old')) {
+if (!exists(precommit +'.old')) {
   fs.unlinkSync(precommit);
 } else {
   fs.writeFileSync(precommit, fs.readFileSync(precommit +'.old'));
